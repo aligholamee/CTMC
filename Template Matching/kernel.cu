1dd99033d9da3e268ae738a2dd7953740c48543d
Ali Gholami <aligholami7596@gmail.com>
2018-07-01 23:46:03 +0430

    Update: Boilerplate to read images and call kernel setup function

--- Template Matching/kernel.cu
+++ Template Matching/kernel.cu
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <cuda_runtime.h>
 
 using namespace std;
+ 
 
 int main(int argc,		// Number of arguments in array argv 
 	char *argv[])	// Array of command-line argument strings
@@ -11,6 +13,40 @@ int main(int argc,		// Number of arguments in array argv
 	for (int count = 0; count < argc; count++)
 		cout << "argv["<< count <<"]: " << argv[count] << endl;
 
+	for (int count = 0; count < argc; count++) {
+
+		// Get BMP file
+		// file
+		// Extract main image matrix
+		// int *h_imageMat = extractMatrix(file)
+
+		// Extract template matrix
+		// int *h_templateMat = extractMatrix(file)
+
+
+		// Call match function
+		// templateMatch(h_imageMat, h_templateMat);
+
+	}
+
 	system("pause");
 	return 0;
 }
+
+int templateMatch(int *h_imageMat, int *h_templateMat)
+{
+
+	// Define device pointers
+	int *d_imageMat;
+	int *d_templateMat;
+
+
+	// Allocate space on device
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
+
