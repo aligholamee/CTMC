6281972bc3c299a187ea7cadac094fcd36f43422
Ali Gholami <aligholami7596@gmail.com>
2018-07-03 17:03:12 +0430

    Update: Properly extracts image matrix

--- template_matching/template_matching/kernel.cu
+++ template_matching/template_matching/kernel.cu
@@ -6,47 +6,60 @@
 
 using namespace std;
 
+void extract_image_matrix(bitmap_image imageBmp, size_t *imageMatrix, size_t width, size_t height);
+void print_image_pixels(size_t* imageMatrix, size_t width, size_t height);
+
 int main()
 {
 	bitmap_image mainImageBmp("collection_coin.bmp");
-	bitmap_image templateImageBmp("template.bmp");
 
-	if (!(mainImageBmp) || !(templateImageBmp)) {
+	if (!(mainImageBmp)) {
 		printf("Error - Failed to open: input.bmp\n");
 		return 1;
 	}
 
-	const unsigned int mainHeight = mainImageBmp.height();
-	const unsigned int mainWidth = mainImageBmp.width();
-	const unsigned int templateHeight = templateImageBmp.height();
-	const unsigned int templateWidth = templateImageBmp.width();
+	size_t mainHeight = mainImageBmp.height();
+	size_t mainWidth = mainImageBmp.width();
+	 
+	size_t total_num_of_pixels_main = mainHeight * mainWidth;
 
-	const unsigned int total_num_of_pixels_main = 3 * mainHeight * mainWidth;
-	const unsigned int total_num_of_pixels_template = 3 * templateHeight * templateWidth;
+	size_t *mainMatrix = new size_t[3 * total_num_of_pixels_main];
 
-
-	char *mainMatrix = new char[total_num_of_pixels_main];
-	char *templateMatrix = new char[total_num_of_pixels_template];
-
+	for (size_t row = 0; row < mainHeight; row++) {
+		for (size_t col = 0; col < mainWidth; col++) {
+			cout << "red: " << int(mainImageBmp.get_pixel(row, col).red);
+			cout << "green: " << int(mainImageBmp.get_pixel(row, col).green);
+			cout << "blue: " << int(mainImageBmp.get_pixel(row, col).blue);
+		}
+		cout << endl;
+	}
+	//extract_image_matrix(mainImageBmp, mainMatrix, mainWidth, mainHeight);
+	//print_image_pixels(mainMatrix, mainWidth, mainHeight);
 
 	system("pause");
 	return 0;
-
-
-
 }
 
-void extract_image_matrix(bitmap_image imageBmp, char *imageMatrix, unsigned int width, unsigned int height)
+void extract_image_matrix(bitmap_image imageBmp, size_t *imageMatrix, size_t width, size_t height)
 {
-	for (int row = 0; row < height; row++) {
-		for (int col = 0; col < width; col++) {
-			char proper_index_r = row * width + col;
-			char proper_index_g = proper_index_r + sizeof(char);
-			char proper_index_b = proper_index_r + 2 * sizeof(char);
+	for (size_t row = 0; row < height; row++) {
+		for (size_t col = 0; col < width; col++) {
+			size_t proper_index_r = row * (width * 3) + col * 3;
+			size_t proper_index_g = proper_index_r + 1;
+			size_t proper_index_b = proper_index_r + 2;
 
-			imageMatrix[proper_index_r] = char(imageBmp.get_pixel(row, col).red);
-			imageMatrix[proper_index_g] = char(imageBmp.get_pixel(row, col).green);
-			imageMatrix[proper_index_b] = char(imageBmp.get_pixel(row, col).blue);
+			imageMatrix[proper_index_r] = imageBmp.get_pixel(row, col).red;
+			imageMatrix[proper_index_g] = imageBmp.get_pixel(row, col).green;
+			imageMatrix[proper_index_b] = imageBmp.get_pixel(row, col).blue;
 		}
+	}
+}
+
+void print_image_pixels(size_t* imageMatrix, size_t width, size_t height)
+{
+	for (size_t row = 0; row < height; row++) {
+		for (size_t col = 0; col < width; col++)
+			cout << imageMatrix[row * (width * 3) + col*3] << " ";
+		cout << endl;
 	}
 }
