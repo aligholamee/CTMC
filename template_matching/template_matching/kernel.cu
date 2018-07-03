b0197f18607adfbb84f876c3d74e9b5e1f768bcd
Ali Gholami <aligholami7596@gmail.com>
2018-07-03 15:03:19 +0430

    Update: Extract image matrix

--- template_matching/template_matching/kernel.cu
+++ template_matching/template_matching/kernel.cu
@@ -1,121 +1,52 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <bitmap_image.hpp>
+#include <iostream>
 
-#include <stdio.h>
-
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
+using namespace std;
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	bitmap_image mainImageBmp("collection_coin.bmp");
+	bitmap_image templateImageBmp("template.bmp");
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	if (!(mainImageBmp) || !(templateImageBmp)) {
+		printf("Error - Failed to open: input.bmp\n");
+		return 1;
+	}
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	const unsigned int mainHeight = mainImageBmp.height();
+	const unsigned int mainWidth = mainImageBmp.width();
+	const unsigned int templateHeight = templateImageBmp.height();
+	const unsigned int templateWidth = templateImageBmp.width();
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	const unsigned int total_num_of_pixels_main = 3 * mainHeight * mainWidth;
+	const unsigned int total_num_of_pixels_template = 3 * templateHeight * templateWidth;
 
-    return 0;
+
+	char *mainMatrix = new char[total_num_of_pixels_main];
+	char *templateMatrix = new char[total_num_of_pixels_template];
+
+
+	system("pause");
+	return 0;
+
+
+
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+void extract_image_matrix(bitmap_image imageBmp, char *imageMatrix, unsigned int width, unsigned int height)
 {
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			char proper_index_r = row * width + col;
+			char proper_index_g = proper_index_r + sizeof(char);
+			char proper_index_b = proper_index_r + 2 * sizeof(char);
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+			imageMatrix[proper_index_r] = char(imageBmp.get_pixel(row, col).red);
+			imageMatrix[proper_index_g] = char(imageBmp.get_pixel(row, col).green);
+			imageMatrix[proper_index_b] = char(imageBmp.get_pixel(row, col).blue);
+		}
+	}
 }
