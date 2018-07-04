56fb856fcd29badeb65971d595c4a0cdc5c95e34
Ali Gholami <aligholami7596@gmail.com>
2018-07-04 09:34:13 +0430

    Update: added CUDA device query

--- template_matching/template_matching/kernel.cu
+++ template_matching/template_matching/kernel.cu
@@ -123,3 +123,33 @@ void save_bitmap_image(string file_name, BITMAP image)
 	fclose(out);
 }
 
+void device_query()
+{
+	const int kb = 1024;
+	const int mb = kb * kb;
+	wcout << "NBody.GPU" << endl << "=========" << endl << endl;
+
+	wcout << "CUDA version:   v" << CUDART_VERSION << endl;
+
+	int devCount;
+	cudaGetDeviceCount(&devCount);
+	wcout << "CUDA Devices: " << endl << endl;
+
+	for (int i = 0; i < devCount; ++i)
+	{
+		cudaDeviceProp props;
+		cudaGetDeviceProperties(&props, i);
+		wcout << i << ": " << props.name << ": " << props.major << "." << props.minor << endl;
+		wcout << "  Global memory:   " << props.totalGlobalMem / mb << "mb" << endl;
+		wcout << "  Shared memory:   " << props.sharedMemPerBlock / kb << "kb" << endl;
+		wcout << "  Constant memory: " << props.totalConstMem / kb << "kb" << endl;
+		wcout << "  Block registers: " << props.regsPerBlock << endl << endl;
+
+		wcout << "  Warp size:         " << props.warpSize << endl;
+		wcout << "  Threads per block: " << props.maxThreadsPerBlock << endl;
+		wcout << "  Max block dimensions: [ " << props.maxThreadsDim[0] << ", " << props.maxThreadsDim[1] << ", " << props.maxThreadsDim[2] << " ]" << endl;
+		wcout << "  Max grid dimensions:  [ " << props.maxGridSize[0] << ", " << props.maxGridSize[1] << ", " << props.maxGridSize[2] << " ]" << endl;
+		wcout << endl;
+	}
+}
+
